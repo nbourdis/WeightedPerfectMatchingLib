26e20432a36112cac1efbd2895b59fecd75ae1f8
Nicolas Bourdis <aldur_disciple@hotmail.com>
2020-11-22 12:20:27 +0100

    Added VS2019 project files

---
+++ INSTALL.bat
@@ -0,0 +1,5 @@
+copy src\wpm\WeightedPerfectMatchingCLib.h install-win32\include\wpm\
+copy src\wpm\WeightedPerfectMatchingLib.h install-win32\include\wpm\
+copy build\WeightedPerfectMatchingLib-Release\WeightedPerfectMatchingLib.lib install-win32\lib
+copy build\WeightedPerfectMatchingLib-Debug\WeightedPerfectMatchingLib-Debug.lib install-win32\lib
+copy build\WeightedPerfectMatchingLib-Debug\WeightedPerfectMatchingLib-Debug.pdb install-win32\lib
